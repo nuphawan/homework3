7e7c4916dcf661018992f2608109a6df27ae5d6b
Opal <nuphawan@yahoo.com>
2019-11-15 00:32:36 -0600

    modified index.html updated to homework3

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+var Password = document.getElementById("Password");
+var submit = document.getElementById("button");
+var copy = document.getElementById("button2");
+var character ="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCCVBNM0123456789!@#$%^&*()_+<>?/;'";
+var Pass = "";
+
+function generate() {
+    for (var i=0; i < length; i++) {
+        var x = Math.floor(Math.random()*character.length);
+        Passs = character[x];
+  
+        return Pass;
+   }
+
+
+ 
+}
+
+//submit.addEventListener("click",generate);
+//clipboard.addEventListener("click",generate);
