231a856fe4d67c54f2f7970d47202f9c2e963206
Opal <nuphawan@yahoo.com>
2019-11-16 13:02:21 -0600

    test commit

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 
 var Password = document.getElementById("Password");
-var submit = document.getElementById("button");
-var copy = document.getElementById("button2");
+var submit = document.querySelector("#button");
+var copy = document.querySelector("#button2");
 var character ="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCCVBNM0123456789!@#$%^&*()_+<>?/;'";
 var Pass = "";
 
 function generate() {
-    for (var i=0; i < length; i++) {
-        var x = Math.floor(Math.random()*character.length);
-        Passs = character[x];
+    for (var i=0; i <= character.length; i++) {
+        var i = Math.floor(Math.random()*character.length);
+        Passs = character[i];
   
         return Pass;
    }
 
-
- 
 }
 
-//submit.addEventListener("click",generate);
-//clipboard.addEventListener("click",generate);
+
+function generate2() {
+    let Password2 = Math.floor(Math.random()*character.length);
+    console.log(Password2);
+}
+ 
+submit.addEventListener("click",generate2);
+clipboard.addEventListener("click",generate);
